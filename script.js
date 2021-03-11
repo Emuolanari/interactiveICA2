2070df789b645f9e7bdc2fc0be1a2a405be9d975
Emuolanari <emuolanari@gmail.com>
2021-03-11 13:47:07 +0000

    cleaned postive attendace array

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const width = 500;
 const height = 500;
-let attendanceArray=[];
+let allAttendanceArray=[];
+let positiveAttendanceArray=[];
+let pStatusAttendance= [];
+let pdgStatusAttendance = [];
 const topo_url = "https://martinjc.github.io/UK-GeoJSON/json/eng/topo_lad.json";
 //const url = "https://bost.ocks.org/mike/map/uk.json";
 const projection = d3.geoMercator().translate([width/2,height/1.4]), path = d3.geoPath(projection);
@@ -33,16 +36,55 @@ Promise.all(files.map(url => d3.json(url))).then(function(values) {
 
     let attendanceData = values[0].attendanceData;
 
-        for (let i=0; i<attendanceData.length; i++){
-            let studentDetails =  attendanceData[i];
-            for (const key in studentDetails) {
-                //console.log(`${key}: ${studentDetails[key]}`);
-                
-                let [studentId,gender,moduleCode,groupId,diffgrp,status,postCode]
-                 =[studentDetails.StudentID,studentDetails.Gender,studentDetails.ModuleCode,
-                    studentDetails.GroupId,studentDetails.DiffGrp,studentDetails.Status,studentDetails.PostalArea];
-                attendanceArray.push({studentId,gender,moduleCode,groupId,diffgrp,status,postCode});
-            }  
+    for (let i=0; i<attendanceData.length; i++){
+        let studentDetails =  attendanceData[i];
+        for (const key in studentDetails) {
+            //console.log(`${key}: ${studentDetails[key]}`);
+            /*let [studentId,gender,moduleCode,groupId,diffgrp,status,postCode]
+                =[studentDetails.StudentID,studentDetails.Gender,studentDetails.ModuleCode,
+                studentDetails.GroupId,studentDetails.DiffGrp,studentDetails.Status,studentDetails.PostalArea];
+            attendanceArray.push({studentId,gender,moduleCode,groupId,diffgrp,status,postCode});*/
+            let [studentId,gender,status,postCode]
+                =[studentDetails.StudentID,studentDetails.Gender,studentDetails.Status,studentDetails.PostalArea];
+                allAttendanceArray.push({studentId,gender,status,postCode});
+        }  
+    }
+        
+    pStatusAttendance = allAttendanceArray.filter(function( obj ) {
+        return obj.status == 'P';
+    });
+    pdgStatusAttendance = allAttendanceArray.filter(function( obj ) {
+        return obj.status == 'PDG';
+    });
+
+
+    positiveAttendanceArray = pStatusAttendance.concat(pdgStatusAttendance);
+    positiveAttendanceArray=positiveAttendanceArray.filter(function( obj ) {
+        return isNaN(obj.postCode.substring(0));
+    });
+    console.log(positiveAttendanceArray);
+    console.log(positiveAttendanceArray[1].postCode.substring(0,2));
+
+
+    /*Let's see how many students live in each area based on postcode
+
+    To do that replace all postcodes starting with 'TS' with 'Middlesbrough' to fit topojson...
+    .see how many unique postcodes we have also to accomplish this and change to respective city names
+
+    First let's count the number of students in the positiveAttendanceArray
+
+    Distance/Region vs Attendace for both male & female
+    */
+    const distinctPostocdesSet = new Set(positiveAttendanceArray.map(e => JSON.stringify(e.postCode.substring(0,2))));
+    const distinctPostocdesArray = Array.from(distinctPostocdesSet).map(e => JSON.parse(e));
+    console.log(distinctPostocdesArray);
+
+    for(let i =0; i<positiveAttendanceArray.length; i++){
+        if (positiveAttendanceArray[i].postCode.substring(0,2) == 'TS'){
+        //replace with Middlesbrough
         }
-        console.log(attendanceArray);
+        else if (positiveAttendanceArray[1].postCode.substring(0,2)=='SR'){
+        //replace with sunderland
+        }
+    }
 });
